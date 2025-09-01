a4ef692f1fbd8220ac345a7d4fc4677c529644a9
abirami2505 <abiramik2503@gmail.com>
2025-09-01 22:57:37 +0530

    Sum of array

---
+++ sumofarray.java
@@ -0,0 +1,11 @@
+public class sumofarray {
+    public static void main(String[] args){
+        int[] num={10,17,45,16,8};
+        int sum=0;
+        for(int i=0;i<num.length;i++){
+            sum+=num[i];
+        }
+        System.out.println(sum);
+    }
+    
+}
