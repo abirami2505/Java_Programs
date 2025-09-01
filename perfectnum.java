57f1490852e08b161f294586f2fdfb0a9817b1ae
abirami2505 <abiramik2503@gmail.com>
2025-09-01 22:32:25 +0530

    Find the given number is perfect or not

---
+++ perfectnum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class perfectnum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum +=i;
+            }
+        }
+        System.out.println(sum);
+        if(num==sum){
+            System.out.println("Perfect number");
+        }
+        else{
+           System.out.println("Not Perfect number"); 
+        }
+    }
+    
+}
