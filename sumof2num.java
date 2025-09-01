564a1599982043c6b40f2db406a667bd79cd7665
abirami2505 <abiramik2503@gmail.com>
2025-09-01 22:56:13 +0530

    Sum of two numbers

---
+++ sumof2num.java
@@ -0,0 +1,18 @@
+import java.util.Scanner ;
+public class sumof2num {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        System.out.println(num1+num2);
+
+    }
+    
+}
+
+//method2                         //methed3
+//while(b!=0){                     int sum= a-(-b);
+    //a++;
+    //b--;
+//}
+//System.out.println(a);
