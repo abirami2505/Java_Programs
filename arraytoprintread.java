38be5ebf8552d647b92e74a8ccffc984ecfdf85e
abirami2505 <abiramik2503@gmail.com>
2025-09-01 22:22:14 +0530

    Print the given array

---
+++ arraytoprintread.java
@@ -0,0 +1,9 @@
+public class arraytoprintread {
+    public static void main (String[] args){
+        int[] numbers={10,20,30};
+        for (int i=0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+        }
+    }
+    
+}
